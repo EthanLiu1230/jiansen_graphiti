58209b2c016c3c9b5a089ece353e59d8fc8ea1f2
YuxLiu <liuyuxuan1230@outlook.com>
2021-05-25 20:19:52 +0800

    category has_many children nullify dependency.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :parent, class_name: 'Category', foreign_key: 'parent_id', optional: true
-  has_many :children, class_name: 'Category', foreign_key: 'parent_id'
+  has_many :children, class_name: 'Category', foreign_key: 'parent_id', dependent: :nullify
 
   validates :name, uniqueness: { scope: :parent_id }
 end
