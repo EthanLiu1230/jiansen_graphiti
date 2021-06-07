09257aadb31fb0f2bff80adc09068ab70c3b365c
YuxLiu <liuyuxuan1230@outlook.com>
2021-06-07 16:21:25 +0800

    [images_controller]refactor to '/:any_record_id/images/:id'

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,19 +1,16 @@
 class ImagesController < ApplicationController
   def create
-    content = Content.find(params.require(:content_id))
-    content.images.attach(params.require(:images))
+    record.images.attach(params.require(:images))
     render status: :created
   end
 
   def destroy
-    content = Content.find(params.require(:content_id))
-    content.images.find(params.require(:id)).purge
+    record.images.find(params.require(:id)).purge
     render status: :ok
   end
 
   def destroy_all
-    content = Content.find(params.require(:content_id))
-    content.images.purge
+    record.images.purge
     render status: :ok
   end
 
@@ -25,5 +22,11 @@ class ImagesController < ApplicationController
   def index
     images = ImageResource.all(params)
     respond_with(images)
+  end
+
+  def record
+    record_type = params.keys.find { |key| key[/_id$/] }.split('_')[0]
+    model = record_type.camelize.constantize
+    model.find(params.require(:"#{record_type}_id"))
   end
 end
